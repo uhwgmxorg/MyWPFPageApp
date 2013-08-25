458afac406bec5f8d1e3715312333f294d6343e7
Uwe ON WS04 <uhw@gmx.org>
2013-08-25 23:50:36 +0200

    Added a comment header in MainWindowView.xaml.cs.

--- MyWPFPageApp/Views/MainWindowView.xaml.cs
+++ MyWPFPageApp/Views/MainWindowView.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/******************************************************************************/
+/*                                                                            */
+/*   Program: MyWPFPageApp                                                    */
+/*   My MVVM Template                                                         */
+/*                                                                            */
+/*   15.8.2013 0.0.0.0 uhwgmxorg Start                                        */
+/*                                                                            */
+/******************************************************************************/
+using System;
 using System.Linq;
 using System.Windows;
 
